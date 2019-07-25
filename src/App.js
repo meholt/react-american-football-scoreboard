1236869694f158771bb1884ef4779691ca6d9ad7
meholt <meholt90@gmail.com>
2019-07-24 20:34:17 -0700

    established-hooks-and-use-state-working-on-buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  let [homeScore, setHomeScore] = useState(0);
+  let [awayScore, setAwayScore] = useState(0);
+
 
   return (
     <div className="container">
@@ -15,12 +18,13 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <h2 homeScore={homeScore} />
+            
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 awayScore={awayScore} />
           </div>
         </div>
         <BottomRow />
